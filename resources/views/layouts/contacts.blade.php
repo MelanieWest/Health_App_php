2198305ab4f64aa9dfc415683bfa228e4104a157
Melanie West <dhmofan@gmail.com>
2017-11-08 14:33:03 -0500

    Improve appearance of some pages.  All is working except update/delete buttons on meds list.  Delete is the next goal

--- resources/views/layouts/contacts.blade.php
+++ resources/views/layouts/contacts.blade.php
@@ -1,17 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<div class="row">
 
-  <div class="col align-self-start">
-   
+
+
+<div class="card" style="width: 400px;">
+
+ <div>
+  <img class="card-img-top rounded mx-auto d-block" src="https://p.apk4fun.com/1a/5b/e9/naveen.oldphonedialer-logo.jpg" style="width:250px; margin:10px;" alt="Card image cap">
+ </div>
+
+  <div class="card-block">
+
+    <div>
+      <h3 class="card-title text-center">Contacts</h3>
+    </div>
+
   </div>
+
+    <div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item text-center"><h3>Friend:   (407)-340-6899</h3></li>
+        <li class="list-group-item text-center"><h3>Doctor:   (407)-221-9696</h3></li>
+        <li class="list-group-item text-center"><h3>Pharmacy: (321)-851-3214</h3></li>
+      </ul>
+    </div>
+
+  <div class="card-block">
+
+    <button class="button" style="margin: 20px; float:right; font-size: 20px;"><a href="/menu">Back</a></button>
+
+    <div>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+              {{ Auth::user()->name }} 
+          </a>
+
+          <ul class="dropdown-menu">
+              <li>
+                  <a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                      Logout
+                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
+              </li>
+          </ul>
+
+    </div>
+
+  </div>
+</div>
+
+
+
+
+@endsection
+
+
+
+
+
+
+<!-- <div class="container">
+<div class="row">
 
   <div class="col align-self-center">
 
 
-  <div>
+            <div>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                         {{ Auth::user()->name }} 
                     </a>
@@ -31,10 +90,6 @@
                     </ul>
 
               </div>
-
-
-
-
 
   <table>
   <thead>
@@ -62,15 +117,18 @@
         </tbody>
       </table>
 
+
       <button class="button" style="margin: 20px;"><a href="/menu">Back</a></button>
 
       </div>
-      
-      <div class="col align-self-end">
-      
-     </div>
- 
-   </div>
+    </div>
  </div>
-  
-@endsection
+   -->
+
+
+
+
+
+
+
+
