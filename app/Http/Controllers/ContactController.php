5d35374520b2ed8de6af234a1b109b95c0fd35f9
Melanie West <dhmofan@gmail.com>
2018-01-25 11:11:02 -0500

    reset 'debug' in config/app to false.  Previous version failed to run, even locally, complaining about quotes needed.  I hope I can find them

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -21,9 +21,9 @@ class ContactController extends Controller
     {
         $auth_user = Auth::user()->id;
         
-       $contact = DB::table('contacts')->where('user_id',$auth_user)->get();
+        $contact = DB::table('contacts')->where('user_id',$auth_user)->get();
         
-       return view('layouts.contacts',compact('contact'));
+        return view('layouts.contacts',compact('contact'));
      //   return view('layouts.contacts');
     }
 
