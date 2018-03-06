55dd7f0b8b3517742dd9081cea34333a8b0dedfb
Melanie West <dhmofan@gmail.com>
2018-03-06 15:21:11 -0500

    connect to database

--- config/database.php
+++ config/database.php
@@ -46,16 +46,17 @@ return [
 
         //below are the amazon credentials (I don't know the password)
         //these are set in the .env file
+        
         //DB_HOST=v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
         //DB_USERNAME=y98mxhkihi3vvaed
         
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bfh2ij19f0qr331r'),
+            'username' => env('DB_USERNAME', 'gib5ece42cggkd9d'),
+            'password' => env('DB_PASSWORD', 'bropbhp396xqo3bb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
